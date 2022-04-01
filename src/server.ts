7d9e7d04b0e3f394ecbb5d7602c5130d712c89dc
Welton <weltonmoura@live.com>
2022-04-01 18:40:43 -0300

    Execicio_02/04

--- src/server.ts
+++ src/server.ts
@@ -1,99 +1,92 @@
-// // console.log('tudo certo meu consagrado!!!')
-
-// // let salario = 1000
-// // let aumento = (salario/100)*25
-// // let novo_salario = salario + aumento
-// // console.log('O novo salario será R$'+novo_salario)
+// // execício_01
+// // require prompt
 const prompt = require("prompt-sync")();
 
-// // let numero: number = prompt('Digite um numero: ');
-
-// // if (numero >= 1) {
-// //     console.log('o numero '+numero+' é positivo.')
-// // } 
-// // else if (numero ==0) {
-// //     console.log('Isso é um zero!')
-// // }
-
-// // else {
-// //     console.log('O numero '+numero+' é negativo.')
-// // }
-// //execicio TypeScript
-   
-
-// let nota_1:number = parseInt(prompt('Digite a primeira nota: '))
-// let nota_2:number = parseInt(prompt('Digite a segunda nota: ')) 
-// let nota_3:number = parseInt(prompt('Digite a terceira nota: '))    
-// let media = (nota_1 + nota_2 + nota_3)/3  
-// // console.log(media)
+// let nota_1:number = parseInt(prompt('Digite a nota do trabalho de laboratório: '))
+// let nota_2:number = parseInt(prompt('Digite a nota da avaliação semestral: ')) 
+// let nota_3:number = parseInt(prompt('Digite a nota do exame final: '))    
+// let media = ((nota_1*2) + (nota_2*3) + (nota_3*5))/(2+3+5)  
+// console.log(media)
 
 // if (media <= 4.9){
-//     console.log("A media do aluno foi "+media+", então o aluno está REPROVADO!")
+//     console.log("A media do aluno foi "+media+", ficando assim com o conceito E")
 // }
 
-// else if(media >= 5 && media <= 6.9){
-//     console.log("A media do aluno foi "+media+", então o aluno está em RECUPERAÇÃO!")
+// else if(media >= 5 && media <= 5.9){
+//     console.log("A media do aluno foi "+media+", ficando assim com o conceito D")
+// }
+// else if(media >= 6 && media <= 6.9){
+//     console.log("A media do aluno foi "+media+", ficando assim com o conceito C")
+// }
+// else if(media >= 7 && media <= 7.9){
+//     console.log("A media do aluno foi "+media+", ficando assim com o conceito B")
 // }
 
-// else if (media>= 7){
-//     console.log("A media do aluno foi " +media+ ", então o aluno está APROVADO!")
+// else if (media>= 8){
+//     console.log("A media do aluno foi " +media+ ", ficando assim com o conceito A")
 // }
 
+// // execício_02
+
+// var numeros = [];
+// numeros.push(parseInt(prompt('Entre com o primeiro numero: ')))
+// numeros.push(parseInt(prompt('Entre com o segundo numero: ')))
+// numeros.push(parseInt(prompt('Entre com o terceiro numero: ')))
+// console.log("Os numeros digitados foram em ordem crescente:\n"+numeros.sort())
 
 
-
-
-
-
-
-
-// let somatotal = 0
-// for (let i = 1; i < 4; i++){
-//     let x = prompt("Digite o "+i+"º numero: ");
-//     somatotal=somatotal+Number(x);
+// execício_03
+// var numeros_c = []
+// var n_1 = parseInt(prompt('Entre com o primeiro numero: '));
+// var n_2 = parseInt(prompt('Entre com o segundo numero: '));
+// while (n_1 > n_2 ) {
+//     console.log("O numero deve ser maior que o anterior")
+//     var n_2 = parseInt(prompt('Entre com o segundo numero: '));
+    
 // }
-// if (somatotal >= 1){
-//     console.log("O numero "+somatotal+" é positivo!(não o notebook kkk)")
+// var n_3 = parseInt(prompt('Entre com o terceiro numero: '));
+
+// while (n_2 > n_3) {
+//     console.log("O numero deve ser maior que o anterior")
+//     var n_3 = parseInt(prompt('Entre com o terceiro numero: '));
 // }
-// else if (somatotal < 0){
-//     console.log("O numero "+somatotal+" é negativo!")
-// }
-// else (somatotal == 0) ;{
-//     console.log("Isso é um zero!!!")
-// }
+// var n_4 = parseInt(prompt('Entre com o quarto numero: '));        
 
-// let i
-// do{
-//     let i = prompt("Digite um numero: ")
-// }
-
-// while(i != 7);
+// numeros_c.push(n_1,n_2,n_3,n_4)
+// console.log("Os numeros digidados em ordem crescente são:\n"+numeros_c.sort())
 
 
-// switch (parseInt(prompt("digite: "))) {
-//     case 1:
-//         console.log("Aluno")
-//         break;
-//     case 2:
-//         console.log("Professor")
-//         break;
-//     case 3:
-//         console.log("Codernador")
-//         break;
-//     default:
-//         console.log("funcionario")
-//         break;
-// }
+// execício_04
+
+var gerente_salario = 3000
+var gerente_aumento = (gerente_salario/100)*10
+var escrituario_salario = 1080
+var escrituario_aumento = (escrituario_salario/100)*50
+var secretario_salario = 1800
+var secretario_aumento = (secretario_salario/100)*35
+var caixa_salario = 2500
+var caixa_aumento = (caixa_salario/100)*20
+var diretor_salario = 5000
+var diretor_aumento = (diretor_salario/100)*10
 
 
-let fruits1: string[] = ['Apple', 'orange','Banana'];
-// console.log(fruits1[1])
-for(let i = 0; i < fruits1.length; i++)
-
-{
-    fruits1[i] = prompt('digite');
-}
-
-for (let i = 0; i < fruits1.length; i++){
-    console.log('A '+(i+1)+'º fruta é '+fruits1[i])
+switch (parseInt(prompt("digite o codigo correspondente: "))) {
+    case 1:
+        console.log("Cargo: Escrituário","\nSalário atual: ",escrituario_salario,"\nValor do aumento: ", escrituario_aumento,"\nNovo salário: ",escrituario_salario+escrituario_aumento);
+            break;
+    case 2:
+        console.log("Cargo: Secretário","\nSalário atual: ",secretario_salario,"\nValor do aumento: ", secretario_aumento,"\nNovo salário: ",secretario_salario+secretario_aumento);
+            break;
+    case 3:
+        console.log("Cargo: Caixa","\nSalário atual: ",caixa_salario,"\nValor do aumento: ", caixa_aumento,"\nNovo salário: ",caixa_salario+caixa_aumento);
+            break;
+    case 4:
+        console.log("Cargo: Gerente","\nSalário atual: ",gerente_salario,"\nValor do aumento: ", gerente_aumento,"\nNovo salário: ",gerente_salario+gerente_aumento);
+            break;
+    case 5:
+        console.log("Cargo: Diretor","\nSalário atual: ",diretor_salario,"\nValor do aumento: Sem aumento\nNovo salário: ",diretor_salario);
+            break;
+    default:
+        console.log('codigo invalido');
+            break;
 }
---
+++ src/server_.ts
@@ -0,0 +1,92 @@
+// // console.log('tudo certo meu consagrado!!!')
+
+// // let salario = 1000
+// // let aumento = (salario/100)*25
+// // let novo_salario = salario + aumento
+// // console.log('O novo salario será R$'+novo_salario)
+// const prompt = require("prompt-sync")();
+
+// // let numero: number = prompt('Digite um numero: ');
+
+// // if (numero >= 1) {
+// //     console.log('o numero '+numero+' é positivo.')
+// // } 
+// // else if (numero ==0) {
+// //     console.log('Isso é um zero!')
+// // }
+
+// // else {
+// //     console.log('O numero '+numero+' é negativo.')
+// // }
+// //execicio TypeScript
+   
+
+// let nota_1:number = parseInt(prompt('Digite a primeira nota: '))
+// let nota_2:number = parseInt(prompt('Digite a segunda nota: ')) 
+// let nota_3:number = parseInt(prompt('Digite a terceira nota: '))    
+// let media = (nota_1 + nota_2 + nota_3)/3  
+// // console.log(media)
+
+// if (media <= 4.9){
+//     console.log("A media do aluno foi "+media+", então o aluno está REPROVADO!")
+// }
+
+// else if(media >= 5 && media <= 6.9){
+//     console.log("A media do aluno foi "+media+", então o aluno está em RECUPERAÇÃO!")
+// }
+
+// else if (media>= 7){
+//     console.log("A media do aluno foi " +media+ ", então o aluno está APROVADO!")
+// }
+
+
+// let somatotal = 0
+// for (let i = 1; i < 4; i++){
+//     let x = prompt("Digite o "+i+"º numero: ");
+//     somatotal=somatotal+Number(x);
+// }
+// if (somatotal >= 1){
+//     console.log("O numero "+somatotal+" é positivo!")
+// }
+// else if (somatotal < 0){
+//     console.log("O numero "+somatotal+" é negativo!")
+// }
+// else (somatotal == 0) ;{
+//     console.log("Isso é um zero!!!")
+// }
+
+// let i
+// do{
+//     let i = prompt("Digite um numero: ")
+// }
+
+// while(i != 7);
+
+
+// switch (parseInt(prompt("digite: "))) {
+//     case 1:
+//         console.log("Aluno")
+//         break;
+//     case 2:
+//         console.log("Professor")
+//         break;
+//     case 3:
+//         console.log("Codernador")
+//         break;
+//     default:
+//         console.log("funcionario")
+//         break;
+// }
+
+
+let fruits1: string[] = ['Apple', 'orange','Banana'];
+// console.log(fruits1[1])
+for(let i = 0; i < fruits1.length; i++){
+    fruits1[i] = prompt('digite'+(i+1)+'º fruta: ');
+}
+
+for (let i = 0; i < fruits1.length; i++){
+    console.log('A '+(i+1)+'º fruta é '+fruits1[i])
+}
+
+
